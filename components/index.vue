d05726f6592a0eeef460fb5bcfaa2dffad28caa9
1131153523@qq.com <fanyihui@tuhu.cn>
2023-09-07 11:30:17 +0800

    release: v0.0.3

--- components/index.vue
+++ components/index.vue
@@ -1,9 +1,9 @@
 <script setup>
-import { ref, onMounted } from 'vue'
-import { Repl, ReplStore } from '@vue/repl'
-import Monaco from '@vue/repl/monaco-editor'
+import { ref, onMounted } from 'vue';
+import { Repl, ReplStore } from '@vue/repl';
+import Monaco from '@vue/repl/monaco-editor';
 import { utoa } from './utils';
-import '@vue/repl/style.css'
+import '@vue/repl/style.css';
 const store = ref();
 const slots = defineSlots();
 
@@ -12,15 +12,24 @@ onMounted(() => {
   const code = children?.[0]?.children;
   const file = {
     'src/App.vue': decodeURIComponent(code)
-  }
+  };
   store.value = new ReplStore({
     serializedState: utoa(JSON.stringify(file))
-  })
-})
+  });
+});
 </script>
 
 <template>
-  <div style="height: 400px;">
-    <Repl v-if="store" autoResize :store="store" :editor="Monaco" :showCompileOutput="true" :clearConsole="false"/>
-  </div>
+  <ClientOnly>
+    <div style="height: 400px">
+      <Repl
+        v-if="store"
+        autoResize
+        :store="store"
+        :editor="Monaco"
+        :showCompileOutput="true"
+        :clearConsole="false"
+      />
+    </div>
+  </ClientOnly>
 </template>
